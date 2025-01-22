c94df8f984ab85f7eda62f5ef32f417c8872aad9
angelica <guidolinangelica66@gmail.com>
2025-01-22 17:18:15 +0100

    init project

---
+++ data/bacheca.js
@@ -0,0 +1,33 @@
+const posts=[
+    {
+       title: "Ciambellone",
+       description: "ecco a voi il ciambellone appena fatto, super buono e super morbido",
+       image:  `http://localhost:${port}/images/ciambellone.jpeg`,
+       tags: ['#delizioso', '#food','#ricetta']
+    },
+    {
+        title: "cracker alla barbabietola",
+        description: "ecco a voi i mie crackers alla barbabietola appena fatti, super buono e super morbido",
+        image: `http://localhost:${port}/images/cracker_barbabietola.jpeg`,
+        tags: ['#delizioso', '#food','#ricetta']
+    },
+    {
+        title: "pane fritto dolce",
+        description: "ecco a voi il mio pane fritto dolce appena fatto, super buono e super morbido",
+        image: `http://localhost:${port}/images/pane_fritto_dolce.jpeg`,
+        tags: ['#delizioso', '#food','#ricetta']
+    },
+    {
+        title: "pasta alla barbabietola",
+        description: "ecco a voi la mia pasta alla barbabietola appena fatta, super buona",
+        image: `http://localhost:${port}/images/pasta_barbabietola.jpeg`,
+        tags: ['#delizioso', '#food','#ricetta']
+    },
+    {
+        title: "torta paesana",
+        description: "ecco a voi la mia torta paesana appena fatta, super buona",
+        image: `http://localhost:${port}/images/torta_paesana.jpeg`,
+        tags: ['#delizioso', '#food','#ricetta']
+    },
+]
+module.exports= posts
